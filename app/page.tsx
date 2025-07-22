a7c075c2bb6c4c1f27420e4ff4bd1436bcdcdb3b
123EFD <erchinqq06@gmail.com>
2025-07-22 16:39:37 +0800

    Upgrade plan subscription

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,14 @@
-import CompanionCard from '@/components/CompanionCard'
-import CompanionsList from '@/components/CompanionsList'
-import CTA from '@/components/CTA'
-import { getAllCompanions } from '@/lib/actions/companion.actions'
-import React from 'react'
-import { getRecentSessions } from '@/lib/actions/companion.actions'
-import { getSubjectColor } from '@/lib/utils'
+import CompanionCard from "@/components/CompanionCard";
+import CompanionsList from "@/components/CompanionsList";
+import CTA from "@/components/CTA";
+import {recentSessions} from "@/constants";
+import {getAllCompanions, getRecentSessions} from "@/lib/actions/companion.actions";
+import {getSubjectColor} from "@/lib/utils";
 
 const Page = async () => {
   const companions = await getAllCompanions({ limit:3 });
   const recentSessionCompanions = await getRecentSessions(10);
+
   return (
     <main>
       <h1>Popular Companions</h1>
@@ -18,22 +18,20 @@ const Page = async () => {
                 <CompanionCard
                     key={companion.id}
                     {...companion}
-                    colors={getSubjectColor(companion.subject)}
+                    color={getSubjectColor(companion.subject)}
                 />
             ))}
             
-
           </section>
 
           <section className='home-section'>
             <CompanionsList
-              title="Recent completed sessions"
+              title="Recently completed sessions"
               companions={recentSessionCompanions}
               classNames="w-2/3 max-lg:w-full"
             />
             <CTA/>
           </section>
-      
     </main>
   )
 }
